e62ed02d3eaeb659ec64d75699eaf89a5cfb6f69
Zaki Berouk <zaki.berouk@etu.unice.fr>
2017-04-26 11:18:54 +0200

    def idea object

---
+++ src/Idea.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+/**
+ * @author Zaki
+ */
+public class Idea {
+
+    private int id;
+    private String description;
+    private List<String> technologies;
+    private String author;
+    private String mail;
+
+}
