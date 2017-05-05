d1bb3bb33075c19718eabbea55c8d9a133d99b4e
Zaki Berouk <zaki.berouk@etu.unice.fr>
2017-05-05 16:25:42 +0200

    client is done

--- src/client/MyClient.java
+++ src/client/MyClient.java
@@ -1,33 +1,32 @@
 package client;
 
+import protocole.Answer;
 import protocole.Request;
-import server.MyServer;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 /**
  * 4 stages of a client's life :
- *  1. Socket's instantiation;
- *  2. Connect the socket with the service server's side targeted;
- *  3. Exchange of data with server's sockets;
- *  4. Close the socket when the communications are done.
+ * 1. Socket's instantiation;
+ * 2. Connect the socket with the service server's side targeted;
+ * 3. Exchange of data with server's sockets;
+ * 4. Close the socket when the communications are done.
  */
 public class MyClient {
 
     private int port;
     private String host;
 
-    public MyClient(int port, String host){
+    public MyClient(int port, String host) {
         this.port = port;
         this.host = host;
     }
 
-    public void start(){
+    public void start() {
         try {
             //Step 1;
             Socket socket = new Socket(host, port);
@@ -37,14 +36,35 @@ public class MyClient {
             ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
 
             //Step 3;
-            while(true){
+            while (true) {
                 Request request = new RequestCreator().create();
+                if (request == null) {
+                    System.out.println("There is no request.");
+                    break;
+                }
+                out.writeObject(request);
+                Answer answer = (Answer) in.readObject();
+                if (!(answer.getStatus().equals(Answer.NORMAL_STATUS))) {
+                    System.out.println("An error occured in the answer.");
+                    return;
+                }
+                if (answer.getAnswer() == null) {
+                    System.out.println("Request successful");
+                } else {
+                    System.out.println("Answer: \n" + answer.getAnswer());
+                }
             }
-
+            socket.close();
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        new MyClient(8080, "localhost").start();
     }
 }
